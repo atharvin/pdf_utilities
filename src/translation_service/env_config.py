071c7fa90fca6db5313b21bfb5f8c1b91bcef85d
atharva <atharva.inamdar511@gmail.com>
2026-04-23 15:38:40 +0530

    replace gcp service account authentication using file with env vars

--- src/translation_service/env_config.py
+++ src/translation_service/env_config.py
@@ -1,8 +1,11 @@
 from decouple import config
 
-credentials_json = config("GCS_CREDENTIALS")
-log_file = config("LOG_FILE_PATH", "test.log")
+log_file = config("LOG_FILE_PATH", "app.log")
 translation_project = config("TRANSLATION_PROJECT", "clean-axiom-299913")
 translation_location = config("TRANSLATION_LOACTION", "us-central1")
 translation_service_creds = config("TRANSLATION_SERVICE_CREDS")
 gcs_output_bucket = config("TRANSLATION_OUTPUT_BUCKET", "nroad-translation-documents")
+translation_private_key = config("TRANSLATION_PRIVATE_KEY")
+translation_private_key_id = config("TRANSLATION_PRIVATE_KEY_ID")
+translation_email = config("TRANSLATION_EMAIL")
+translation_client_id = config("TRANSLATION_CLIENT_ID")
--- src/translation_service/translate_utils.py
+++ src/translation_service/translate_utils.py
@@ -1,3 +1,4 @@
+import base64
 import traceback
 import fitz
 
@@ -28,6 +29,15 @@ def translate_text(
     )
     return results
 
+def decode_base64(base64_string):
+    # Add padding
+    padding = len(base64_string) % 4
+    if padding:
+        base64_string += '=' * (4 - padding)
+    base64_bytes = base64_string.encode('utf-8')
+    input_bytes = base64.b64decode(base64_bytes)
+    input_string = input_bytes.decode('utf-8')
+    return input_string
 
 def translate_pdf_sync(
     file_path: bytes,
@@ -42,8 +52,22 @@ def translate_pdf_sync(
         project = ec.translation_project
         location = ec.translation_location
         logger.info(f"Configuring translation client in {project=} at {location=}")
-        credentials = service_account.Credentials.from_service_account_file(
-            ec.translation_service_creds,
+        credentials_dict = {
+            "type": "service_account",
+            "project_id": ec.translation_project,
+            "private_key_id": ec.translation_private_key_id,
+            "private_key": decode_base64(ec.translation_private_key),
+            "client_email": ec.translation_email,
+            "client_id": ec.translation_client_id,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/cloud-translation-api-user%40clean-axiom-299913.iam.gserviceaccount.com",
+            "universe_domain": "googleapis.com"
+        }
+
+        credentials = service_account.Credentials.from_service_account_info(
+            credentials_dict,
             scopes=["https://www.googleapis.com/auth/cloud-platform"],
         )
         client = translate.TranslationServiceClient(credentials=credentials)
@@ -53,6 +77,7 @@ def translate_pdf_sync(
             line.strip() for line in traceback.format_exc().splitlines() if line.strip()
         )
         logger.error(f"Error in configuring translation client: {tb}")
+        raise
 
     try:
         src = fitz.open(stream=file_path, filetype="pdf")
